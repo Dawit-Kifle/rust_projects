ffe21872c69f00762564c9a005be77b02666a455
alphakys <gys1120@naver.com>
2023-09-28 14:26:26 +0900

    9.28 test driven development

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -180,36 +180,60 @@ fn show(pt: &Point) {
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+//
+// struct Config {
+//     // In structs use owned values, such as String.
+//     // In structs use owned values, such as String.
+//     // In structs use owned values, such as String.
+//     // In strcuts use owned values, such as String.
+//     // In structs use owned values, such as String.
+//     query: String,
+//     file_path: String
+// }
+//
+// impl Config {
+//     // [GUIDELINE] In structs use owned values, such as String.
+//     fn build(args: &[String]) -> Result<Config, &'static str> {
+//         if args.len() < 3 {
+//             return Err("not enough arguments");
+//             //panic!("not enough arguments");
+//         }
+//
+//         let query = args[1].clone();
+//         let file_path = args[2].clone();
+//
+//         Ok(Config { query, file_path })
+//     }
+// }
+//
+// fn run(config: Config) -> Result<(), Box<dyn Error>>{
+//     let contents = fs::read_to_string(config.file_path)?;
+//     //.expect("Should have been able to read the file");
+//     println!("With text: \n{}", contents);
+//     Ok(())
+// }
 
-struct Config {
-    // In structs use owned values, such as String.
-    // In structs use owned values, such as String.
-    // In structs use owned values, such as String.
-    // In strcuts use owned values, such as String.
-    // In structs use owned values, such as String.
-    query: String,
-    file_path: String
-}
+use std::process;
+use minigrep::Config;
 
-impl Config {
-    // [GUIDELINE] In structs use owned values, such as String.
-    fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config { query, file_path }
-    }
-}
 fn main() {
 
     let args: Vec<String> = env::args().collect();
-    // println!("{:?}", args);
-    let config = Config::new(&args);
 
-    println!("{} {}", config.query, config.file_path);
+    let config = Config::build(&args).unwrap_or_else(|err|{
+        println!("Probelm parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config){
+        println!("Application error : {e}");
+        process::exit(1);
+    }
+
+
 
     // let mut file =  File::create("poem.txt");
     // file.expect("What's wrong").write_all(b"I'm nobody! Who are you?
