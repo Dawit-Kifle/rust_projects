bf42477b08e4379308beb9d094f6d0700b5f1e14
alphakys <gys1120@naver.com>
2023-10-02 18:13:34 +0900

    env arguments

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -284,10 +284,34 @@ fn try_create(paragraph: &str) -> Option<S> {
     }
 }
 
+pub fn search<'a> (query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+    results
+}
 
 fn main() {
 
 
+    let args: Vec<String> = env::args().collect();
+
+    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+
+    // println!("{:?}", config);
 
 //     let contents = "\
 // Rust:
