8396835049cc33c445044c68270200268d4d0952
Dawit-Kifle <160421085+Dawit-Kifle@users.noreply.github.com>
2024-03-04 03:31:21 +0000

    test 3

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+
+fn get_names(names: Vec<&'static str>) -> Vec<&'static str> {
+
+  names.iter()
+    .map(|s| s.to_string()) 
+    .collect()
 }
+
+
+fn main() {
+  let a = vec![1, 2, 3];
+  let b = vec![4, 5, 6];
+
+  let mut sum = vec![0; a.len()];
+
+  for i in 0..a.len() {
+    sum[i] = a[i] + b[i];
+  }
+
+  println!("Sum vector is: {:?}", sum);
+
+  let x: f32 = 1.5;
+ let y = 2.3; // y is f64 by default
+ let sum = x + y;
+ println!("Sum is {}", sum);
+
+ let x = 1.5;
+ let y: f32 = x as f32;
+let x: f32 = 1.5; 
+let y = x as f64;
+let x = 5;
+let _y = x as f32; 
+
+let z = 6.7 as i32;
+println!("{}", z);
+
+let names = vec!["Sam", "Sara"];
+let name_list = get_names(names);
+
+
+}
+
+
+
