0d9ee0319ecd09ed8753f6bedf260fc68aaa5777
Dawit-Kifle <160421085+Dawit-Kifle@users.noreply.github.com>
2024-03-04 17:58:52 +0000

    delete project

--- hello_world/target/debug/deps/hello_world-63db71e2032dbcc4.d
+++
@@ -1,5 +0,0 @@
-/workspaces/rust_projects/hello_world/target/debug/deps/hello_world-63db71e2032dbcc4: src/main.rs
-
-/workspaces/rust_projects/hello_world/target/debug/deps/hello_world-63db71e2032dbcc4.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/workspaces/rust_projects/hello_world/target/debug/hello_world: /workspaces/rust_projects/hello_world/src/main.rs
